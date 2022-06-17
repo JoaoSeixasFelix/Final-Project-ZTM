6685441f4d30f7d0e7d2c6d4a5b35c80f1b7fff4
João Paulo <joaopaulo_seixas@outlook.com>
2022-06-16 21:33:47 -0300

    update tool and predictions home page  - V23 - ZTM

---
+++ .next/server/chunks/330.js
@@ -0,0 +1,105 @@
+"use strict";
+exports.id = 330;
+exports.ids = [330];
+exports.modules = {
+
+/***/ 5255:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/strategy-svgrepo-com.f9a4f7ef.svg","height":512,"width":512});
+
+/***/ }),
+
+/***/ 7543:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "z": () => (/* binding */ Button)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9003);
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(classnames__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const Button = ({ children , onClick , type , disabled =false , width , height , backGroundColor , textColor , border , borderColor , borderRadius , margin , marginTop , marginRight , marginBottom , marginLeft , padding , paddingTop , paddingBottom , paddingRight , paddingLeft , rounded , effects  })=>{
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("button", {
+            onClick: onClick,
+            type: type,
+            className: classnames__WEBPACK_IMPORTED_MODULE_1___default()(`${backGroundColor}
+           ${textColor}
+           ${width}
+           ${height}
+           ${border}
+           ${borderColor}
+           ${borderRadius}
+           ${margin}
+           ${marginTop}
+           ${marginRight}
+           ${marginBottom}
+           ${marginLeft}
+           ${padding}
+           ${paddingTop}
+           ${paddingBottom}
+           ${paddingRight}
+           ${paddingLeft}
+           ${rounded}
+           ${effects}
+           font-bold
+           rounded-full font-Poppins`),
+            children: children
+        })
+    });
+};
+
+
+/***/ }),
+
+/***/ 5740:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "I": () => (/* binding */ Input)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9003);
+/* harmony import */ var classnames__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(classnames__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const Input = ({ onValueChange , name , placeholder , value ="" , type , width , bgColor , textColor , borderColor , margin , marginTop , marginRight , marginBottom , marginLeft , padding , required  })=>{
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("input", {
+            onChange: (e)=>onValueChange(e.target.value)
+            ,
+            name: name,
+            placeholder: placeholder,
+            value: value,
+            type: type,
+            required: true,
+            className: classnames__WEBPACK_IMPORTED_MODULE_1___default()(`${borderColor}
+           ${bgColor}
+           ${textColor}
+           ${width}
+           ${margin}
+           ${marginTop}
+           ${marginRight}
+           ${marginBottom}
+           ${marginLeft}
+           ${padding}
+           font-bold rounded-lg text-xs
+           shadow appearance-none border-2 
+          leading-tight focus:outline-nonefirst-letter:focus:shadow-outline`)
+        })
+    });
+};
+
+
+/***/ })
+
+};
+;
